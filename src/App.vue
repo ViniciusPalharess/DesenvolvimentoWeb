d9773ce1619f7eb040189ec46beb5de788a63d97
Viniciusdepaulapalhares <depaulapalharesv@gmail.com>
2023-05-14 03:47:51 -0300

    commit12

--- src/App.vue
+++ src/App.vue
@@ -120,7 +120,6 @@ const carrinhos = ref({
 
  items: [],
  total: 0,
-
 });
 
 function adicionar(produto) {
@@ -197,6 +196,10 @@ function removerItem(item) {
   
 }
 
+function limparCarrinho() {
+  carrinhos.value = { items: [], total: 0 };
+}
+
 function selecionarProduto(produto) {
   if (produto.id === produtoSelecionado.value.id) {
     mostrarDescricao.value = !mostrarDescricao.value;
@@ -236,6 +239,7 @@ function toggleCarrinho() {
             <button class="voltarprodutos" @click="toggleCarrinho">Voltar para Produtos</button>
 
           </div>
+          <button class="button-5" @click="limparCarrinho()">Limpar Carrinho</button>
           <p  class="total" > Total: {{ carrinhos.total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) }}</p>
         </div>
       </div>
@@ -316,12 +320,56 @@ a {
 }
 
 .total{
-display: flex;
+display: flex;  
 justify-content: center;
 font-size: 21px;
 font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
 
+.button-5 {
+  align-items: center;
+  background-clip: padding-box;
+  background-color: #fa6400;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: system-ui,-apple-system,system-ui,"Helvetica Neue",Helvetica,Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  justify-content: center;
+  line-height: 1.25;
+  margin: 0;
+  min-height: 3rem;
+  padding: calc(.875rem - 1px) calc(1.5rem - 1px);
+  position: relative;
+  text-decoration: none;
+  transition: all 250ms;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: baseline;
+  width: auto;
+}
+
+.button-5:hover,
+.button-5:focus {
+  background-color: #fb8332;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
+}
+
+.button-5:hover {
+  transform: translateY(-1px);
+}
+
+.button-5:active {
+  background-color: #c85000;
+  box-shadow: rgba(0, 0, 0, .06) 0 2px 4px;
+  transform: translateY(0);
+}
 /* Produto */
 .produtos {
  display: flex;
